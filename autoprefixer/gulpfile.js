0a4bce508b67e8006643941a7eb2b6885c64e5a5
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-07-30 15:23:34 -0400

    gulp file and autoprefixer

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('styles',function() {
+gulp.task('styles', async function(){
   gulp.src('css/styles.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('build'))
 });
 
-
+//this automatically compiles vs having to run gulp watch everytime.
 gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+  gulp.watch('css/styles.css', gulp.series('styles'));
 });
