75e6b3722e9a20c59edc2e294a0e165d62ff45ca
Ed Heil <edheil@fastmail.fm>
2021-03-20 18:41:55 -0400

    Update to gulp 4.0
    * Update gulp as 3.9 doesn't work with newer versions of node
    * Update gulpfile to be 4.0 compatible
    * Update styles.css with the results of a more recent autoprefixer run

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('styles',function() {
+gulp.task('styles',function(done) {
   gulp.src('css/styles.css')
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build'));
+  done();
 });
 
 
 gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+  gulp.watch('css/styles.css', gulp.series('styles'));
 });
